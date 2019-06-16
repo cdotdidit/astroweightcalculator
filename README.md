d59bb195120bba098e71b64cd895b9ba890db841
cash <cash@CREAM.local>
2019-06-15 17:00:07 -0700

    this is a completed astrocalculator

---
+++ README.md
@@ -0,0 +1,9 @@
+<!-- This is a calculator that will tell you, if you enter your weight in the empty box, how much you would weigh on any given planet (that you choose from the drop down box). 
+
+This calculator uses JavaScript and NPM.
+
+In your terminal, you will test your code by typing npm test and run the calculator to the local server by typing npm start.
+
+Enjoy.
+
+-->
--- main.js
+++ main.js
@@ -1,1 +1,49 @@
 // Write your JavaScript code here!
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+  ];
+
+  
+  planets.forEach(function(element){
+      var dropdown = document.getElementById('planets');
+      var option = document.createElement('option');
+      var txt = document.createTextNode(element[0]);
+      option.appendChild(txt);
+      dropdown.insertBefore(option,dropdown.lastChild);
+    });
+
+    
+    var namePlanet = document.getElementById("planets").value;
+
+function calculateWeight(userWeight, planetName){
+    var userWeight = document.getElementById("user-weight").value;
+    var planetName = document.getElementById("planets").selectedIndex;
+    var gravity = planets[planetName][1];
+    var result = userWeight * gravity;
+    return result;
+};
+ 
+
+  function handleClickEvent() {
+    var userWeight = document.getElementById("user-weight").value;
+    var planetName = document.getElementById("planets").value;
+    var result = calculateWeight(userWeight, planetName);
+
+        document.getElementById("output").innerHTML = "If you were on " + planetName+ ", you would weigh " + result+ "lbs!";
+    }
+
+
+  
+  // Bonus Challenges
+  // 8. Reverse the drop down order so that the sun is first.
+  // 9. Make it look nice using bootstrap (http://getbootstrap.com/getting-started/)  
